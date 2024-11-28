d7484cf86f8d734ed8707e135c3df07f53331eb0
rodofer2020 <61705774+rodofer2020@users.noreply.github.com>
2024-11-27 22:53:18 -0600

    Update streamlit_app.py
    Connected changes needes to move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 import pandas as pd
 from snowflake.snowpark.functions import col
 
+cnx=st.connection("snowflake")
+session=cnx.session()
 #import matplotlib.pyplot as plt
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw: ")
